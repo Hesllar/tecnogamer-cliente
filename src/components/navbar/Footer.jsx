7e8cb70eefb83d844f69ad8595488dc5bf91d7e2
snow <smashdojo3@gmail.com>
2022-06-28 22:43:02 -0400

    se crea y se implementa footer

---
+++ src/components/navbar/Footer.jsx
@@ -0,0 +1,16 @@
+
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer id="footer">   
+        <div className="container py-4">
+            <div className="copyright">
+                <strong><span>TecnoGamer</span></strong>. Todos Los Derechos Reservados 2022
+            </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
