0d01c7ddbb796ca05718e80feba984d37b22cd4d
Hesllar Henry <hesllar.linzmayer@gmail.com>
2022-07-05 00:13:00 -0400

    Creación de lista de productos

--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -1,8 +1,45 @@
+import axios from 'axios';
+import { useState,useEffect } from 'react';
+ import {Table} from 'react-bootstrap';
+const url = 'http://localhost:8000/api/v0/allproduct';
+
 
 export const Products = () => {
+
+  const [product, setProduct] = useState([]);
+
+  const listProduct = async() =>{
+
+      const res = await axios.get(url);
+
+      const {Data} = res.data;
+
+      setProduct(Data)
+  }
+
+    useEffect(() => {
+    listProduct();
+  }, [])
+  
+  console.log(product)
   return (
     <>
-        <h1>Productos</h1>
+        <Table striped bordered hover variant="dark">
+                            <thead>
+                                <tr>
+                                    <th>Titulo</th>
+                                    <th>Stock</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {product.map((pro,index)=> (
+                                    <tr key={pro._id}>
+                                        <td>{pro.nombreProducto}</td>
+                                        <td >{pro.stock}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </Table>
     </>
   )
 }
