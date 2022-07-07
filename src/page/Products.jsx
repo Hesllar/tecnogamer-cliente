ddd366da5f71e4cd1566642c723500e716a97a0b
Hesllar Henry <hesllar.linzmayer@gmail.com>
2022-07-07 08:59:01 -0400

    Modificación en componente Products

--- src/page/Products.jsx
+++ src/page/Products.jsx
@@ -1,49 +1,48 @@
 import axios from 'axios';
-import { useState,useEffect } from 'react';
- import {Container,Row,Card,Button} from 'react-bootstrap';
-const url = 'http://localhost:5000/api/v0/allproduct';
+import { useState, useEffect } from 'react';
+import { Container, Row, Card, Button, Col } from 'react-bootstrap';
+const url = 'http://localhost:8000/api/v0/allproduct';
 
 
 export const Products = () => {
 
-  const [product, setProduct] = useState([]);
+    const [product, setProduct] = useState([]);
 
-  const listProduct = async() =>{
+    const listProduct = async () => {
 
-      const res = await axios.get(url);
+        const res = await axios.get(url);
 
-      const {Data} = res.data;
+        const { Data } = res.data;
 
-      setProduct(Data)
-  }
+        setProduct(Data)
+    }
 
     useEffect(() => {
-    listProduct();
-  }, [])
-  
-  console.log(product)
-  return (
-    <>
-    <Container>
-        <Row>
-            {product.map((pro)=> (
-                <Card style={{ width: '18rem' }}>
-                    <Card.Body>
-                        <Card.Title>{pro.nombreProducto}</Card.Title>
-                        <label >{pro.stock}</label>
-                        <Card.Text>
-                            {pro.descripcion}
-                        </Card.Text>
-                    </Card.Body>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <label>{pro.precio}</label>
-                        <Button variant="primary">carrito</Button>
-                    </Card.Body>
-                </Card>
-            ))}
-        </Row>
-    </Container>
-    </>
-  )
+        listProduct();
+    }, [])
+
+
+    return (
+        <>
+            <Container>
+                <Row className='mt-2 mb-2'>
+                    {product.map((pro) => (
+                        <Col key={pro._id} className='col-md-4 '>
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Body>
+                                    <Card.Title> {pro.nombreProducto}</Card.Title>
+                                    <strong >STOCK:<label className='fw-normal'>{pro.stock}</label></strong>
+                                </Card.Body>
+                                <Card.Img variant="top" src="holder.js/100px180" />
+                                <Card.Body>
+                                    <label>${pro.precio}</label>
+                                </Card.Body>
+                                <Button variant="primary">carrito</Button>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+        </>
+    )
 }
