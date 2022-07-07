e53518f3b6b439de716b780f211bc5e0ebe27cba
Hesllar Henry <hesllar.linzmayer@gmail.com>
2022-07-07 08:55:45 -0400

    Se crear titulo de crear producto en el menú

--- src/components/navbar/Header.jsx
+++ src/components/navbar/Header.jsx
@@ -1,39 +1,40 @@
 
-import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
-import {NavLink} from 'react-router-dom';
-import {routes}  from '../../routes/config-route';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { routes } from '../../routes/config-route';
 import logo from '../../img/tecnogamelogo.png'
 
 export const Header = () => {
   return (
-    
+
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand  as={NavLink} to="/" ><img src={logo} 
+      <Navbar.Brand as={NavLink} to="/" ><img src={logo}
         width="40"
         height="40"
         className="d-inline-block align-top"
-        alt=""/> TecnoGamer
+        alt="" /> TecnoGamer
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link as={NavLink} to={routes.home} >Inicio</Nav.Link>
           <Nav.Link as={NavLink} to={routes.products}>Productos</Nav.Link>
-          <NavDropdown  
-              id="nav-dropdown-dark-example"
-              menuVariant="dark" 
-              title='Categoria'>
-            <NavDropdown.Item  as={NavLink} to={routes.mouseTeclado} >Mouse y Teclados</NavDropdown.Item>
-            <NavDropdown.Item  as={NavLink} to={routes.gabinete} >Gabinetes</NavDropdown.Item>
+          <Nav.Link as={NavLink} to={routes.addProduct}>Crear Producto</Nav.Link>
+          <NavDropdown
+            id="nav-dropdown-dark-example"
+            menuVariant="dark"
+            title='Categoria'>
+            <NavDropdown.Item as={NavLink} to={routes.mouseTeclado} >Mouse y Teclados</NavDropdown.Item>
+            <NavDropdown.Item as={NavLink} to={routes.gabinete} >Gabinetes</NavDropdown.Item>
             <NavDropdown.Item left as={NavLink} to={routes.memoria} >Memoria</NavDropdown.Item>
           </NavDropdown>
         </Nav>
         <Nav>
           <Nav.Link as={NavLink} to={routes.login} >Iniciar Sesion</Nav.Link>
-          <Nav.Link as={NavLink} to={routes.register} >Registrarse</Nav.Link> 
-          <Nav.Link  >Cerrar Sesion</Nav.Link> 
+          <Nav.Link as={NavLink} to={routes.register} >Registrarse</Nav.Link>
+          <Nav.Link  >Cerrar Sesion</Nav.Link>
         </Nav>
       </Navbar.Collapse>
-  </Navbar>
+    </Navbar>
   )
 }
