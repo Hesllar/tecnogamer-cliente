49281a48e1220cac699d33c17e4ec1781c9a7214
Hesllar Henry <hesllar.linzmayer@gmail.com>
2022-07-07 08:59:32 -0400

    Se agrega router de CrearProducto

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,28 +2,30 @@ import { Routes, Route } from 'react-router-dom';
 import { Layout } from '../components/layouts/layaout';
 import { HomePage } from '../page/HomePage';
 import { Products } from '../page/Products';
-import {MouseTeclado} from '../page/categoria/MouseTeclado';
-import {Gabinete} from '../page/categoria/Gabinete';
-import {Memoria} from '../page/categoria/Memoria';
-import { routes }  from './config-route';
-import { LoginPage }  from '../page/user/LoginPage';
-import { RegisterPage }  from '../page/user/RegisterPage';
-import  NotFoundPages  from '../page/errores/NotFoundPage';
+import { MouseTeclado } from '../page/categoria/MouseTeclado';
+import { Gabinete } from '../page/categoria/Gabinete';
+import { Memoria } from '../page/categoria/Memoria';
+import { routes } from './config-route';
+import { LoginPage } from '../page/user/LoginPage';
+import { RegisterPage } from '../page/user/RegisterPage';
+import NotFoundPages from '../page/errores/NotFoundPage';
+import { CrearProducto } from '../page/CrearProducto';
 
 export const AppRouter = () => {
   return (
     <Routes>
-        <Route path={routes.home} element={<Layout />}>
-            <Route  index element={<HomePage />}/>
-            <Route  path={routes.products} element={<Products />}/>
-            <Route  path={routes.mouseTeclado} element={<MouseTeclado />}/>
-            <Route  path={routes.gabinete} element={< Gabinete/>}/>
-            <Route  path={routes.memoria} element={<Memoria />}/>
-            <Route  path={routes.login} element={<LoginPage />}/>
-            <Route  path={routes.register} element={<RegisterPage />}/>
-        </Route>
-          {/* Ventana de errores  */}
-        <Route exact path="*" element={<NotFoundPages/>}/>
+      <Route path={routes.home} element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path={routes.products} element={<Products />} />
+        <Route path={routes.mouseTeclado} element={<MouseTeclado />} />
+        <Route path={routes.gabinete} element={< Gabinete />} />
+        <Route path={routes.memoria} element={<Memoria />} />
+        <Route path={routes.login} element={<LoginPage />} />
+        <Route path={routes.register} element={<RegisterPage />} />
+        <Route path={routes.addProduct} element={<CrearProducto />} />
+      </Route>
+      {/* Ventana de errores  */}
+      <Route exact path="*" element={<NotFoundPages />} />
     </Routes>
   )
 }
